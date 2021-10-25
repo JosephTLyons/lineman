d1dbf6e803fa197b3bf49974647ed07abbe190d9
Joseph T Lyons <JosephTLyons@gmail.com>
2021-10-25 02:30:34 -0400

    Rename application

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use structopt::StructOpt;
 use walkdir::WalkDir;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "basic")]
-struct StwArgs {
+#[structopt(name = "lineman")]
+struct LinemanArgs {
+    /// The root path from which to begin processing
     #[structopt(short, long)]
     path: PathBuf,
 
@@ -21,7 +22,7 @@ fn main() -> Result<(), String> {
     // let cleaned_files: Vec<String> = Vec::new();
     // let files_with_errors: Vec<String> = Vec::new();
 
-    let args = StwArgs::from_args();
+    let args = LinemanArgs::from_args();
 
     for dir_entry_result in WalkDir::new(args.path) {
         match dir_entry_result {
@@ -135,7 +136,6 @@ fn test_runner(input_output_lines_array: &[(&str, &str)]) {
 // TODO:
 
 // Fix all bad error handling - don't use string errors and don't use unwraps - some errors might be killing the program when the program could just continue on
-// Better program name
 // Better logging - Log what has been checked, what has actually been changed, and what couldn't be changed, for whatever reason
 // Show numerical stats on how many files were looked at, how many were changed, duration of run, etc
 // Tweak command line argument parsing (help, info, etc)
